0ca9826630c5f8e68a904df57fe1b9266d24a3bc
Akshaya H <81792372+my-my414@users.noreply.github.com>
2021-09-04 13:26:28 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("Enter your number :");
+    scanf("%d",&num);
+    if(num%2==0)
+    {
+        printf("Your number is even");
+        
+    }
+    else
+    {
+        printf("your number is odd");
+    }
+}
